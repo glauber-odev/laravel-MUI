f88b10cc5a606e523fa46e3439d7a0f82179bff5
glauber-odev <111056180+glauber-odev@users.noreply.github.com>
2025-03-04 16:13:27 -0300

    update: layout and register page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,15 +1,47 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        // link do curso: https://youtu.be/mUhCequxk6I?si=PUg6m1hWD6GdsMBf
+        <div
+        style={{
+            minHeight: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'relative',
+            overflow: 'hidden'
+        }}
+        >
+        <div
+        style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: 'url(../../images/students-background.jpeg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            zIndex: -2,
 
+        }}
+        >
+
+        <div
+        style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0,0,0, 0.3)',
+            zIndex: -2,
+        }}
+        ></div>
+
+        </div>
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
