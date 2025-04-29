8f2af8e88ad4942a91301e2318ede10b74876f4a
Glauber Vieira <111056180+glauber-odev@users.noreply.github.com>
2025-04-29 16:47:44 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        \URL::forceRootUrl(\Config::get('app.url'));
+        if (str_contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+        }
     }
 }
